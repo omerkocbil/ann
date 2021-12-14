f3567d62697e4c2f758ccf50e3a636525a441885
omerkocbil <omer-95@hotmail.com>
2021-12-15 00:59:25 +0300

    feat(layer): edit init method with argument assignments
    Edit __init__ method with argument assignments in Dense class.
    
    Assign neuron, summation, and activation arguments to class-level
    variables with the same names. In addition, call .py files from other
    modules within the Dense class in order to perform the necessary
    operations related to these arguments.
    
    Refs ANN-3

--- layer_omer/dense.py
+++ layer_omer/dense.py
@@ -1,11 +1,18 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))))
 from init_omer.initializer import initializer
+from summation_omer.initializer import summation as summate
+from activation_omer.initalizer import activation as activate
 
 class Dense():
 
     def __init__(self, neuron=4, weight_init='uniform', bias_init='zeros', 
                  summation='sum', activation='relu'):
+        self.neuron = neuron
         self.weight_init = initializer[weight_init]
         self.bias_init = initializer[bias_init]
+        self.summation = summate[summation]
+        self.activation = activate[activation]
+        
