7503c68e130ff0e4e931c30700a12d6150090973
omerkocbil <omer-95@hotmail.com>
2021-12-15 20:22:36 +0300

    feat(layer): add the layer's weights and bias features
    Add the layer's weights and bias features. In Dense class, within the
    __init__ method define the weights and bias properties that help the
    layers learn about the network they should have.
    
    Refs ANN-8

--- layer_omer/dense.py
+++ layer_omer/dense.py
@@ -8,11 +8,14 @@ from activation_omer.initalizer import activation as activate
 
 class Dense():
 
-    def __init__(self, neuron=4, weight_init='uniform', bias_init='zeros', 
+    def __init__(self, neuron=4, weight_init='uniform', bias_init='zero', 
                  summation='sum', activation='relu'):
         self.neuron = neuron
         self.weight_init = initializer[weight_init]
         self.bias_init = initializer[bias_init]
         self.summation = summate[summation]
         self.activation = activate[activation]
+        
+        self.weights = []
+        self.bias = None
         
