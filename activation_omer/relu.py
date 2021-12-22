e08a234f3b675510e765c4e5a60d694310ab619e
omerkocbil <omer-95@hotmail.com>
2021-12-22 18:10:04 +0300

    feat(activation): edit build() method in ReLu class
    Edit build() method in ReLu class.
    
    This commit;
    - rename build() method with forward(),
    - From now on, the function only returns the maximum value between the 0
    it performs and the value from the argument if the argument it receives
    is an integer,
    - If the argument it takes is a numpy array, it makes the largest
    comparison between each element of the array and 0 and returns an array.

--- activation_omer/relu.py
+++ activation_omer/relu.py
@@ -5,5 +5,8 @@ class ReLu():
     def __init__(self):
         return
     
-    def build(self, output):
-        return np.max(0, output)
+    def forward(self, output):
+        if isinstance(output, int):
+            return np.max(0, output)
+        elif isinstance(output, np.ndarray):
+            return np.maximum(np.zeros(output.size), output)
