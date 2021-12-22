36be840ae959c6d2a7f503f54f8d7c8375ecd97a
omerkocbil <omer-95@hotmail.com>
2021-12-22 17:52:56 +0300

    feat(summation): edit build() method in Sum class
    Edit build() method in Sum class.
    
    This commit;
    - rename build() method with forward(),
    - From now on, the function only adds the elements in the list it
    performs if the argument it receives is one-dimensional,
    - If the argument it takes is two-dimensional, does addition at axis
    level 1 and return a one-dimensional array.

--- summation_omer/sum.py
+++ summation_omer/sum.py
@@ -1,10 +1,11 @@
-import numpy as np
-import operator
 
 class Sum():
     
     def __init__(self):
         return
     
-    def build(self, wn_xn):
-        return 0 if not wn_xn else wn_xn[0] + self.build(wn_xn[1:])
+    def forward(self, wn_xn):
+        if wn_xn.ndim == 1:
+            return 0 if not wn_xn else wn_xn[0] + self.build(wn_xn[1:])
+        elif wn_xn.ndim == 2:
+            return wn_xn.sum(axis=1)
