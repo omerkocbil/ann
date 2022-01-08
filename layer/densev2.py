51b3755ca709a303669c93285c9b64cd712eb791
Taha Üzümcü <36675318+kinkintama@users.noreply.github.com>
2022-01-08 15:53:07 +0300

    dense layer for pytorch like network

---
+++ layer/densev2.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import numpy as np
+
+sys.path.insert(0, os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))))
+from layer_initialization.initializer import initializer
+from summation.initializer import summation as summate
+from activation.initalizer import activation as activate
+
+
+class Dense():
+
+    def __init__(self, input_size=4, output_size=4, weight_init='uniform', bias_init='zero',
+                 summation='sum', activation='relu'):
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.weights = self.initialize_weight(weight_init)
+        self.bias = self.initialize_bias(bias_init)
+        self.summation = summate[summation]
+        self.activation = activate[activation]
+
+
+    def initialize_weight(self, weight_init):
+        size = (self.input_size, self.output_size)
+        weights =  initializer[weight_init].build(size[::-1])
+        return weights
+
+    def initialize_bias(self, bias_init):
+        return initializer[bias_init].build() #TODO one bias per layer? not neuron?
+
+    def __call__(self, input):
+        w_x = np.multiply(input, self.weights)
+        output = self.summation.forward(w_x)
+        output = output + self.bias
+        output = self.activation.forward(output)
+        return output
