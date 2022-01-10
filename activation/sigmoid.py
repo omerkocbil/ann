62f6ebc56bcda9ab5c6f11f8f1d989d05cf43980
omerkocbil <omer-95@hotmail.com>
2022-01-11 00:17:08 +0300

    feat(activation): add sigmoid backward
    Add backward() function in Sigmoid class in sigmoid.py file to calculate
    the derivative of net values.
    
    ANN-15

--- activation/sigmoid.py
+++ activation/sigmoid.py
@@ -7,3 +7,6 @@ class Sigmoid():
     
     def forward(self, net):
         return (1 / (1 + np.exp(-net)))
+    
+    def backward(self, net):
+        return self.forward(net) * (1-self.forward(net))
