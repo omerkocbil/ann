c5f13ce3b33093bfd5fc308cb32bb36c5be2f75a
omerkocbil <omer-95@hotmail.com>
2022-01-11 13:05:17 +0300

    feat(loss): add binary cross-entropy loss backward
    Add backward() function in BinaryCrossEntropy class in
    binary_cross_entropy.py file to calculate the derivative of loss values.
    
    ANN-16

--- loss/binary_cross_entropy.py
+++ loss/binary_cross_entropy.py
@@ -6,11 +6,7 @@ class BinaryCrossEntropy():
         return
     
     def calculate(self, y, yhat):
-        return -(y*np.log(yhat) + ((1-y)*np.log(1-yhat)))
+        return -((y * np.log(yhat)) + ((1-y) * np.log(1-yhat)))
     
     def backward(self, y, yhat):
-        return -2 * np.mean(np.subtract(y, yhat))
-
-
-bce = BinaryCrossEntropy()
-bce.calculate(np.array([0, 1]), np.array([0.8, 0.8]))
+        return (-y/yhat) + ((1-y) / (1-yhat))
