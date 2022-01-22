3062b548e816fca3c38d2d2f6f057bb5f5fdcfef
omerkocbil <omer-95@hotmail.com>
2022-01-22 22:13:20 +0300

    feat(model): add bias backward and updation
    Add bias backward and updation code in network.py file in backward()
    function and update_params() function. Also define dbiases class variable
    to update bias. And test this changes in test.py file.
    
    Refs ANN-18

--- model/test.py
+++ model/test.py
@@ -27,7 +27,7 @@ model.config(loss='binary_crossentropy', optimizer=SGD)
 X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
 y = [[1], [1], [1], [0]]
 
-for epoch in range(2500):
+for epoch in range(10000):
     for i, x in enumerate(X):
         yhat = model.forward(x)
         print(yhat, end=' ')
@@ -35,7 +35,7 @@ for epoch in range(2500):
         loss = model.loss(y[i], yhat)
         #print('loss:', loss)
 
-        dw, w = model.backward(y[i], yhat)
+        dw, w, db, b = model.backward(y[i], yhat)
         #print('dweights:', dw)
         #print('weights:', w)
     
